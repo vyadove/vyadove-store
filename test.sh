ccc61bbd812c91c2a836011d210646890a5a778a
ArseniyDev <jkl@usa.com>
2025-03-17 10:33:02 +0200

    feat: remove test files for HTTP server and Stripe payment integration as they are no longer needed in the project

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-stripe payment_intents create  \
-  --amount=1099 \
-  --currency=usd \
-  --receipt-email="I.override.your.customer.email.settings@example.com" \
-  -d "metadata[orderId]"=6735
