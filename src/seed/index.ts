4d2aa7369c8f54f4defcaa31649c9a736ccff2c8
ArseniyDev <jkl@usa.com>
2025-04-20 09:34:42 +0300

    Add seed data for products, collections, and globals; update store settings
    - Introduced new JSON files for products, collections, and globals.
    - Updated the global settings for the store with name and currency.
    - Created media entries and seeded product data into the database.
    - Implemented asynchronous creation of collections and products from JSON files.

--- src/seed/index.ts
+++ src/seed/index.ts
@@ -1,21 +1,58 @@
 import { getPayload } from "payload";
 import config from "@payload-config";
+import products from "./products.json";
+import collections from "./collections.json";
+import globals from "./globals.json";
 
 const seed = async () => {
     // Get a local copy of Payload by passing your config
     const payload = await getPayload({ config });
 
-    const product = await payload.create({
-        collection: "products",
+    await payload.updateGlobal({
+        slug: "store-settings",
         data: {
-            title: "My Homepage",
-            variants: [
-                {
-                    price: 100,
-                }
-            ]
-            // other data to seed here
-        },
+            name: "ShopNex",
+            currency: "USD",
+        }
+    });
+
+    await payload.create({
+        collection: "media",
+        data: {
+            alt: "watch",
+            updatedAt: "2025-04-20T04:59:11.486Z",
+            createdAt: "2025-04-20T04:59:11.487Z",
+            url: "https://pub-e0a548fa3e234baf8e41a8fd95bb8ad5.r2.dev/shopnex-images/media/watch-hero.png",
+            thumbnailURL: "https://pub-e0a548fa3e234baf8e41a8fd95bb8ad5.r2.dev/shopnex-images/media/watch-hero.png",
+            filename: "watch-3.png",
+            mimeType: "image/png",
+            filesize: 1077498,
+            width: 1024,
+            height: 1024,
+            focalX: 50,
+            focalY: 50
+        }
+    })
+
+    globals.forEach(async (global: any) => {
+        await payload.updateGlobal({
+            slug: global.globalType,
+            data: global,
+        });
+    });
+
+    collections.forEach(async (collection: any) => {
+        await payload.create({
+            collection: "collections",
+            data: collection,
+        });
+    });
+
+    products.forEach(async (product: any) => {
+        await payload.create({
+            collection: "products",
+            data: product,
+        });
     });
 };
 
