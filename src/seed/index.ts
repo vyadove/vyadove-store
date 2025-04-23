af29022e6a52e71fca0873a7cce07a8a8d25d776
ArseniyDev <jkl@usa.com>
2025-04-23 13:29:53 +0300

    refactor: streamline seed script by consolidating media creation and updating global data

--- src/seed/index.ts
+++ src/seed/index.ts
@@ -13,45 +13,57 @@ const seed = async () => {
         data: {
             name: "ShopNex",
             currency: "USD",
-        }
+        },
     });
 
-    await payload.create({
+    const backgroundImage = await payload.create({
         collection: "media",
         data: {
             alt: "watch",
             updatedAt: "2025-04-20T04:59:11.486Z",
             createdAt: "2025-04-20T04:59:11.487Z",
             url: "https://pub-e0a548fa3e234baf8e41a8fd95bb8ad5.r2.dev/shopnex-images/media/watch-hero.png",
-            thumbnailURL: "https://pub-e0a548fa3e234baf8e41a8fd95bb8ad5.r2.dev/shopnex-images/media/watch-hero.png",
+            thumbnailURL:
+                "https://pub-e0a548fa3e234baf8e41a8fd95bb8ad5.r2.dev/shopnex-images/media/watch-hero.png",
             filename: `watch-${new Date().getTime()}.png`,
             mimeType: "image/png",
             filesize: 1077498,
             width: 1024,
             height: 1024,
             focalX: 50,
-            focalY: 50
-        }
-    })
+            focalY: 50,
+        },
+    });
 
     globals.forEach(async (global: any) => {
         await payload.updateGlobal({
             slug: global.globalType,
-            data: global,
+            data: { ...global, backgroundImage: backgroundImage.id },
         });
     });
 
-    collections.forEach(async (collection: any) => {
-        await payload.create({
-            collection: "collections",
-            data: collection,
-        });
-    });
+    const collectionResults = await Promise.all(
+        collections.map(async (collection: any) => {
+            return payload.create({
+                collection: "collections",
+                data: collection,
+            });
+        })
+    );
 
-    products.forEach(async (product: any) => {
+    const productCollectionIds = [
+        collectionResults[0].id,
+        collectionResults[0].id,
+        collectionResults[0].id,
+        collectionResults[1].id,
+        collectionResults[1].id,
+        collectionResults[2].id,
+    ];
+
+    products.forEach(async (product: any, index) => {
         await payload.create({
             collection: "products",
-            data: product,
+            data: { ...product, collections: productCollectionIds[index] },
         });
     });
 };
