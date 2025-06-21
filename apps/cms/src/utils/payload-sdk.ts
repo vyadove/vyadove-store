ad380893bc2426a4accbdc031a49a1043a47f9b6
ArseniyDev <jkl@usa.com>
2025-06-21 14:08:36 +0300

    chore: update Config import path and enhance fetch handling for browser compatibility

--- apps/cms/src/utils/payload-sdk.ts
+++ apps/cms/src/utils/payload-sdk.ts
@@ -1,7 +1,10 @@
-import type { Config } from "@/payload-types";
+import type { Config } from "@shopnex/types";
 
 import { PayloadSDK } from "@shopnex/payload-sdk";
 
+const isBrowser = typeof window !== "undefined";
+
 export const payloadSdk = new PayloadSDK<Config>({
     baseURL: `${process.env.NEXT_PUBLIC_SERVER_URL}/api`,
+    fetch: isBrowser ? (...args) => window.fetch(...args) : undefined,
 });
