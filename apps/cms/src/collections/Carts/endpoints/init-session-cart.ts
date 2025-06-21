72ed08b674b61070120df4ad68f9554b9c051c9b
ArseniyDev <jkl@usa.com>
2025-06-21 19:12:04 +0300

    chore: implement initSessionCart endpoint for cart session management

---
+++ apps/cms/src/collections/Carts/endpoints/init-session-cart.ts
@@ -0,0 +1,40 @@
+import {
+    Endpoint,
+    generateCookie,
+    getCookieExpiration,
+    mergeHeaders,
+} from "payload";
+
+export const initSessionCart: Endpoint = {
+    handler: async (req) => {
+        const sessionId = crypto.randomUUID();
+        const cart = await req.payload.create({
+            collection: "carts",
+            data: {
+                cartItems: [],
+                sessionId,
+            },
+        });
+        const cartCookie = generateCookie({
+            name: "cart-session",
+            expires: getCookieExpiration({ seconds: 60 * 60 * 24 * 30 }),
+            path: "/",
+            returnCookieAsObject: false,
+            value: sessionId,
+        });
+
+        const newHeaders = new Headers({
+            "Set-Cookie": cartCookie as string,
+        });
+
+        req.responseHeaders = req.responseHeaders
+            ? mergeHeaders(req.responseHeaders, newHeaders)
+            : newHeaders;
+
+        return Response.json({
+            sessionId,
+        });
+    },
+    method: "get",
+    path: "/",
+};
