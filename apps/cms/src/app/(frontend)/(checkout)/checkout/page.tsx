64d8c8035e708504a3d6a6424d8e70ae9a630abd
ArseniyDev <jkl@usa.com>
2025-06-27 07:41:01 +0300

    chore: add dynamic export to Checkout component for forced dynamic rendering

--- apps/cms/src/app/(frontend)/(checkout)/checkout/page.tsx
+++ apps/cms/src/app/(frontend)/(checkout)/checkout/page.tsx
@@ -13,6 +13,8 @@ export const metadata: Metadata = {
     title: "Checkout",
 };
 
+export const dynamic = "force-dynamic";
+
 export default async function Checkout() {
     const cart = await getCart();
 
