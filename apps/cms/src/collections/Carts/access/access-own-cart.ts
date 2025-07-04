bd2055f93adb9b7befed46a0902060f55a9d8253
ArseniyDev <jkl@usa.com>
2025-07-04 21:28:06 +0300

    refactor: move parseCookies call to improve code clarity in canAccessOwnCart function

--- apps/cms/src/collections/Carts/access/access-own-cart.ts
+++ apps/cms/src/collections/Carts/access/access-own-cart.ts
@@ -2,13 +2,12 @@ import { checkRole } from "@/access/roles";
 import { parseCookies } from "payload";
 
 export const canAccessOwnCart = ({ req }: any) => {
-    const cookies = parseCookies(req.headers);
-
     if (req.user) {
         if (checkRole(["admin"], req.user)) {
             return true;
         }
     }
+    const cookies = parseCookies(req.headers);
 
     const sessionId = cookies.get("cart-session");
     if (!sessionId) {
