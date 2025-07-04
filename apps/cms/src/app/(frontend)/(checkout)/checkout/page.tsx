d8aa9014053c9b9a774146405536e0342011312f
ArseniyDev <jkl@usa.com>
2025-07-04 21:23:59 +0300

    refactor: simplify Checkout component by removing unused imports and async logic

--- apps/cms/src/app/(frontend)/(checkout)/checkout/page.tsx
+++ apps/cms/src/app/(frontend)/(checkout)/checkout/page.tsx
@@ -1,13 +1,8 @@
 import type { Metadata } from "next";
 
-import { getCart } from "@/app/api/services/cart";
-import { retrieveCustomer } from "@/app/api/services/customers";
-import { notFound } from "next/navigation";
 import React from "react";
 
-import PaymentWrapper from "../../_components/checkout/payment-wrapper";
-import CheckoutForm from "../../_templates/checkout-form";
-import CheckoutSummary from "../../_templates/checkout-summary";
+import CheckoutPage from "../../_templates/checkout/checkout-page";
 
 export const metadata: Metadata = {
     title: "Checkout",
@@ -15,21 +10,10 @@ export const metadata: Metadata = {
 
 export const dynamic = "force-dynamic";
 
-export default async function Checkout() {
-    const cart = await getCart();
-
-    if (!cart) {
-        return notFound();
-    }
-
-    const customer = await retrieveCustomer();
-
+export default function Checkout() {
     return (
-        <div className="grid grid-cols-1 small:grid-cols-[1fr_416px] content-container gap-x-40 py-12">
-            <PaymentWrapper cart={cart} data-testid="payment-wrapper">
-                <CheckoutForm cart={cart} customer={customer} />
-            </PaymentWrapper>
-            <CheckoutSummary cart={cart} />
+        <div>
+            <CheckoutPage />
         </div>
     );
 }
