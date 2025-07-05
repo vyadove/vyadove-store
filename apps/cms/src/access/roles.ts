163b864c1a08a16cdaff6d060d872ecef3ed362e
ArseniyDev <jkl@usa.com>
2025-07-05 10:07:17 +0300

    feat: extend FieldAccessArgs type to include siblingData and blockData properties

--- apps/cms/src/access/roles.ts
+++ apps/cms/src/access/roles.ts
@@ -1,10 +1,11 @@
+import type { FieldAccessArgs } from "@/admin/types";
 import type { User } from "@/payload-types";
 import type { Access, AccessArgs } from "payload";
 
 export const checkRole = (roles: User["roles"] = [], user?: null | User) =>
     !!user?.roles?.some((role) => roles?.includes(role));
 
-type isAdmin = (args: AccessArgs<User>) => boolean;
+type isAdmin = (args: AccessArgs<User> | FieldAccessArgs<User>) => boolean;
 
 export const admins: isAdmin = ({ req: { user } }) => {
     return checkRole(["admin"], user);
--- apps/cms/src/admin/types.ts
+++ apps/cms/src/admin/types.ts
@@ -1,4 +1,5 @@
 import type {
+    JsonObject,
     PayloadRequest,
     RequestContext,
     SanitizedCollectionConfig,
@@ -35,3 +36,31 @@ export type BeforeChangeHook<T extends TypeWithID = any> = (args: {
     originalDoc?: T;
     req: PayloadRequest;
 }) => any;
+
+export type FieldAccessArgs<
+    TData extends TypeWithID = any,
+    TSiblingData = any,
+> = {
+    /**
+     * The data of the nearest parent block. If the field is not within a block, `blockData` will be equal to `undefined`.
+     */
+    blockData?: JsonObject | undefined;
+    /**
+     * The incoming, top-level document data used to `create` or `update` the document with.
+     */
+    data?: Partial<TData>;
+    /**
+     * The original data of the document before the `update` is applied. `doc` is undefined during the `create` operation.
+     */
+    doc?: TData;
+    /**
+     * The `id` of the current document being read or updated. `id` is undefined during the `create` operation.
+     */
+    id?: number | string;
+    /** The `payload` object to interface with the payload API */
+    req: PayloadRequest;
+    /**
+     * Immediately adjacent data to this field. For example, if this is a `group` field, then `siblingData` will be the other fields within the group.
+     */
+    siblingData?: Partial<TSiblingData>;
+};
