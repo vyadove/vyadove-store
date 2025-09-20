f3f4c0f6646f2b1103d05411527b5aa284e036c4
ArseniyDev <jkl@usa.com>
2025-09-20 15:20:57 +0300

    remove: old create-shopnex-app

--- packages/create-shopnex-app/src/constants.ts
+++
@@ -1,5 +0,0 @@
-import chalk from "chalk";
-
-export const REPO_URL = "https://github.com/shopnex-ai/shopnex.git"; // User provided repo
-export const ORG_NAME = "ShopNex";
-export const PAYLOAD_VERSION = "3.31.0";
